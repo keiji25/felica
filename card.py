dc014df14046af9e09ebeae8463ad93e1fcf92fd
keiji <k019c1084@it-neec.jp>
2022-05-22 18:55:11 +0900

    外部制御&出力フォーマット以外完成かも

--- card.py
+++ card.py
@@ -1,38 +1,39 @@
 import os
 import sys
 import datetime
-import time
 import playsound
 import nfc
 
-
-
+r = []
 
 def print_info(s, student_info):
     if s == "出発":
         print(f"\033[33m{s}モード\033[0m")
     elif s == "時間未経過":
         print(f"\033[31m{s}モード\033[0m")
-    print(f"\t　学籍番号：{student_info[0]}")
-    print(f"\t　　　氏名：{student_info[1]}")
-    print(f"\tカレッジ名：{student_info[2]}")
-    print(f"\t　　学科名：{student_info[3]}")
-    print(f"\t　　　性別：{student_info[4]}")
-    print(f"\t　　　学年：{student_info[5]}")
+    print_list = [
+        f"\t　学籍番号：{student_info[0]}\n",
+        f"\t　　　氏名：{student_info[1]}\n",
+        f"\tカレッジ名：{student_info[2]}\n",
+        f"\t　　学科名：{student_info[3]}\n",
+        f"\t　　　性別：{student_info[4]}\n",
+        f"\t　　　学年：{student_info[5]}\n"
+    ]
+    return print_list
 
 def check_card(student_id):
     path = './inout.gsheet'
     if not os.path.isfile(path):
-        with open(path, 'w', encoding='shift_jis') as f:
+        with open(path, 'w', encoding='utf-8') as f:
             pass
         
-    with open(path, 'r+', encoding='shift_jis') as f:
+    with open(path, 'r+', encoding='utf-8') as f:
         index = f.readlines()
         now = datetime.datetime.now()
         kaishi_flag, kitaku_flag = False, False
         cnt_num = 0
         for i in index:
-            if student_id in i:
+            if student_id in i and str(now).split( )[0] in i:
                 cnt_num += 1
 
         if cnt_num % 2 == 0:
@@ -47,7 +48,8 @@ def check_card(student_id):
                     break
             diff = str(now - bef).split(":")
             if int(diff[0]) >= 1 or int(diff[1]) >= 30:
-                kitaku_flag = True
+                    kitaku_flag = True            
+                
         os.system('cls')
         with open("student.csv", 'r', encoding='utf-8') as a:
                 index = a.readlines()
@@ -58,15 +60,16 @@ def check_card(student_id):
         if kitaku_flag or kaishi_flag:
             f.writelines(f"{student_id}, {s}, {now}\n")
             # 出力
-            print_info(s, student_info)
+            r = print_info(s, student_id)
             playsound.playsound("OK.mp3")
         else:
-            print_info("時間未経過", student_info)
-            print(f"残り\033[33m{30 - int(diff[1])}分後\033[0mに終了できます")
-            
+            r = [print_info("時間未経過", student_info), f"残り\033[33m{30 - int(diff[1])}分後\033[0mに終了できます"]
+    
+    return r
             
 
 def connected(tag):
+    global r
     service_code = 0x200B
     try: 
         if isinstance(tag, nfc.tag.tt3.Type3Tag):
@@ -75,7 +78,7 @@ def connected(tag):
                 blcd = nfc.tag.tt3.BlockCode(0,service=0)
                 block_data = tag.read_without_encryption([svcd], [blcd])
                 student_id = str(block_data[0:9].decode("utf-8"))
-                check_card(student_id)
+                r = check_card(student_id)
             except Exception as e:
                 print("Error:%s" % e)
     except AttributeError:
@@ -83,18 +86,16 @@ def connected(tag):
     return True
 
 def main():
-    print("ウォークラリー時間管理だぉ\nすたーーーーと！\n")
-    print("-------------------------------------------\n")
+    global r
     try:
         clf = nfc.ContactlessFrontend('usb')
-        while True:
-            clf.connect(rdwr={'on-connect': connected,})
+        clf.connect(
+            rdwr={'on-connect': connected}
+        )
     except OSError as e:
         print("PaSoRiが認識されませんでした")
         print(f"エラー： {e}")
-        for i in range(10, 0, -1):
-            print(f"\r{i}秒後に自動終了します　　", end="")
-            time.sleep(1)
+        
     except KeyboardInterrupt:
         clf.close()
         sys.exit(0)
@@ -104,6 +105,8 @@ def main():
         print("考えられる原因")
         print("\tPaSoliのセットアップが正しくできていない")
         print("\tパソコンのやる気がない")
-        for i in range(10, 0, -1):
-            print(f"\r{i}秒後に自動終了します", end="")
-            time.sleep(1)
+    clf.close()
+    return r
+
+# main()
+
--- cli.py
+++ cli.py
@@ -1,36 +1,54 @@
 import tkinter
 import card
+import datetime
+
 def func():
     getvalue = textBox1.get()
-    print("in the function =",getvalue)
-    textBox1.delete(0,tkinter.END)
-    label2["text"] = getvalue
+    textBox1.delete(0, tkinter.END)
+    if s_btn.cget("text") == " 実行中 ":
+        print(s_btn.cget("text"))
+        label2["text"] = card.main()
+        btn.after(100, func)
 
 def calc(event):
     getvalue = textBox1.get()
-    textBox1.delete(0,tkinter.END)
-    label2["text"] = getvalue
-    card.main()
+    textBox1.delete(0 ,tkinter.END)
+    if s_btn.cget("text") == " 実行中 ":
+        label2["text"] = card.main()
+        btn.after(100, func)
+        textBox1.bind('<Return>', calc)
 
+def change():
+    getvalue = s_btn.cget("text")
+    if getvalue == "停止中":
+        s = " 実行中 "
+    elif getvalue == " 実行中 ":
+        s = "停止中"
+    s_btn["text"] = s
+    
 
 root = tkinter.Tk()
 root.title(u"入退場管理")
 root.geometry("500x300") 
 
 # 入出力エリア
+s_btn = tkinter.Button(text='停止中', command=change)
+s_btn.pack(anchor='center')
+
 textBox1 = tkinter.Entry() 
 textBox1.place(width=100, height=30)
+textBox1.pack(anchor='center')
 
 
 label2 = tkinter.Label(font=("Helvetica",14))
+label2.pack()
 textBox1.focus_set()
+
 btn = tkinter.Button(text='実行', command=func)
+
+if s_btn.cget("text") == " 実行中 ":
+    btn.after(100, func)
 
 textBox1.bind('<Return>', calc)
 
-textBox1.pack(anchor='center')
-btn.pack()
-label2.pack()
-
-
-root.mainloop() 
+root.mainloop()
